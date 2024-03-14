e36eedf7d93d40d93c7c92f91e05ccd9e2e93e54
Eduarda M. dos Santos <eduardamorads@gmail.com>
2024-03-14 09:02:58 -0300

    Delete day01 directory

--- day01/exercises/printing.py
+++
@@ -1,5 +0,0 @@
-print("A 'single quote' inside a double quote")
-print('A "double quote" inside a single quote')
-print("Alternatively you can just \"escape\" the quote")
-
-# https://replit.com/@eduardamorads/printing
