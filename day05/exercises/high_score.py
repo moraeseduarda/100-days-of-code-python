97880e6f3798ca837e15c2ae37eb71ab77925daf
Eduarda <eduardamorads@gmail.com>
2024-03-19 23:01:29 -0300

    Update high_score.py

--- day05/exercises/high_score.py
+++ day05/exercises/high_score.py
@@ -10,13 +10,13 @@
 # Input a list of student scores
 student_scores = input().split()
 for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
+    student_scores[n] = int(student_scores[n])
 
 # Write your code below this row 👇
 highest_score = 0
 
 for score in student_scores:
-  if score > highest_score:
-    highest_score = score
+    if score > highest_score:
+        highest_score = score
 
 print(f"The highest score in the class is: {highest_score}")
