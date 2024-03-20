757798ab44a58336a01c41b9be105092ab6b57bf
Eduarda <eduardamorads@gmail.com>
2024-03-19 22:52:10 -0300

    Update debugging_practice.py

--- day01/exercises/debugging_practice.py
+++ day01/exercises/debugging_practice.py
@@ -8,5 +8,3 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
-
-# https://replit.com/@eduardamorads/debuggingpractice
