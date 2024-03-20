7d782df5060e063f02dafb57cfd9b1d33a496adb
Eduarda <eduardamorads@gmail.com>
2024-03-19 23:00:28 -0300

    Update average_height.py

--- day05/exercises/average_height.py
+++ day05/exercises/average_height.py
@@ -22,7 +22,7 @@
 # Transforming each item in the list as an integer value
 student_heights = input().split()
 for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n]) 
+    student_heights[n] = int(student_heights[n]) 
 # 🚨 Don't change the code above 👆
   
 # Write your code below this row 👇 
@@ -31,14 +31,14 @@ total_height = 0
 
 # sum()
 for height in student_heights:
-  total_height += height
+    total_height += height
 print(f"total height = {total_height}")
 
 total_students = 0
 
 # len()
 for student in student_heights:
-  total_students += 1
+    total_students += 1
 print(f"number of students = {total_students}")
 
 average_height = round(total_height / total_students)
