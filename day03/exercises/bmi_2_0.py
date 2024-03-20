b6dfc2729d64e6f73f67e437cecbd29a2d2d7533
Eduarda <eduardamorads@gmail.com>
2024-03-19 22:51:53 -0300

    Update bmi_2_0.py

--- day03/exercises/bmi_2_0.py
+++ day03/exercises/bmi_2_0.py
@@ -25,14 +25,12 @@ weight = int(input())
 bmi = weight / (height**2)
 
 if bmi < 18.5:
-	print(f"You BMI is {bmi}, you are underweight.")
+    print(f"You BMI is {bmi}, you are underweight.")
 elif bmi < 25:
-	print(f"Your BMI is {bmi}, you have a normal weight.")
+    print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi < 30:
-	print(f"Your BMI is {bmi}, you are slightly overweight.")
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi < 35:
-	print(f"Your BMI is {bmi}, you are obese.")
+    print(f"Your BMI is {bmi}, you are obese.")
 else:
-	print(f"Your BMI is {bmi}, you are severely obese")
-
-# https://replit.com/@eduardamorads/bmi2
+    print(f"Your BMI is {bmi}, you are severely obese")
