e47db3d29ab08b68a0086a95e46bb0e6f39a6ba2
Eduarda <eduardamorads@gmail.com>
2024-03-19 22:54:32 -0300

    Update leap_year.py

--- day03/exercises/leap_year.py
+++ day03/exercises/leap_year.py
@@ -39,14 +39,12 @@ year = int(input())
 # Write your code below this line 👇
 
 if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print("Leap year")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
     else:
-      print("Not leap year")
-  else:
-    print("Leap year")
+        print("Leap year")
 else:
-  print("Not leap year")
-  
-# https://replit.com/@eduardamorads/leapyear
+    print("Not leap year")
