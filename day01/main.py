ed901de4fc45253d375d65d9b88bc8fdacc1faad
Eduarda <eduardamorads@gmail.com>
2024-03-19 22:53:09 -0300

    Update main.py

--- day01/main.py
+++ day01/main.py
@@ -9,5 +9,3 @@ name_pet = input("What's your pet's name?\n")
 band_name = name_city + " " + name_pet
 
 print("Your band name could be " + band_name)
-
-# https://replit.com/@eduardamorads/band-name-generator
