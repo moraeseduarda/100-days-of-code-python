7444391ae5930cdf3ab522fc20c8252710f99d01
moraeseduarda <eduardamorads@gmail.com>
2024-07-21 16:32:44 -0300

    changed day18 main.py

--- day18/main.py
+++ day18/main.py
@@ -9,11 +9,22 @@ sam_the_triangle = Turtle()
 # sam_the_triangle = turtle.Turtle()
 sam_the_triangle.shape("triangle")
 sam_the_triangle.color("dark blue")
+sam_the_triangle.pencolor("green")
 
 # TODO - Challenge 1: Draw a Square.
-for i in range(4):
-    sam_the_triangle.forward(100)
-    sam_the_triangle.right(90)
+# for i in range(4):
+#     sam_the_triangle.forward(100)
+#     sam_the_triangle.right(90)
+
+# TODO - Challenge 2: Draw a Dashed Line.
+# for i in range(10):
+#     sam_the_triangle.forward(10)
+#     sam_the_triangle.penup()
+#     sam_the_triangle.forward(10)
+#     sam_the_triangle.pendown()
+
+# TODO - Challenge 3: Drawing Different Shapes
+
 
 screen = Screen()
 # screen = turtle.Screen()
