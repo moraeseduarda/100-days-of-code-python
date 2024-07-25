f2099a0286cfa443a0db1afbd344cbcab91312ff
moraeseduarda <eduardamorads@gmail.com>
2024-07-25 14:45:11 -0300

    updated day18 main.py

--- day18/exercises/challenge_5.py
+++ day18/exercises/challenge_5.py
@@ -1,12 +1,37 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
 sam_the_triangle = Turtle()
-
+turtle.colormode(255)
 sam_the_triangle.shape("triangle")
-sam_the_triangle.color("dark blue")
-sam_the_triangle.pencolor("green")
+sam_the_triangle.speed("fastest")
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    new_color = (r, g, b)
+    return new_color
+
 
 # TODO - Challenge 5: Turtle Challenge 5 - Draw a Spirograph
 
+# My solution
+for _ in range(80):
+    sam_the_triangle.color(random_color())
+    sam_the_triangle.circle(100)
+    sam_the_triangle.left(5)
+
+# def draw_spirograph(size_of_gap):
+#     for _ in range(int(360 / size_of_gap)):
+#         sam_the_triangle.color(random_color())
+#         sam_the_triangle.circle(100)
+#         current_heading = sam_the_triangle.heading()
+#         sam_the_triangle.setheading(current_heading + 10)
+
+
+# draw_spirograph(5)
 screen = Screen()
 screen.exitonclick()
--- day18/main.py
+++ day18/main.py
@@ -1,1 +1,44 @@
 # The Hirst Painting Project
+import turtle as t
+import colorgram
+import random
+
+timmy = t.Turtle()
+timmy.hideturtle()
+timmy.speed("fastest")
+timmy.penup()
+t.colormode(255)
+colors = colorgram.extract("image.jpg", 25)
+
+# rgb_colors = []
+# for color in colors:
+#
+#     red = color.rgb.r
+#     green = color.rgb.g
+#     blue = color.rgb.b
+#     current_color = (red, green, blue)
+#     rgb_colors.append(current_color)
+
+rgb_colors = [(229, 228, 226), (225, 223, 224), (199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57),
+              (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174),
+              (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42),
+              (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203)]
+
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    timmy.dot(20, random.choice(rgb_colors))
+    timmy.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
