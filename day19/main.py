5d93921dc2138dd742b0b9b395785baf1957365f
moraeseduarda <eduardamorads@gmail.com>
2024-08-04 18:09:30 -0300

    updated day19 main project

--- day19/main.py
+++ day19/main.py
@@ -1,36 +1,42 @@
 # Turtle Racing Game
-import random
-import turtle
 import turtle as t
 import random as r
 
-screen = turtle.Screen()
+screen = t.Screen()
+screen.setup(width=500, height=400)
 list_of_turtles = []
 colors = ["purple", "blue", "green", "yellow", "orange", "red"]
+is_race_on = False
 
 for i in range(0, 6):
     color = colors[i]
-    new_turtle = t.Turtle()
+    new_turtle = t.Turtle(shape="turtle")
     new_turtle.color(color)
-    new_turtle.shape("turtle")
+    new_turtle.penup()
     list_of_turtles.append(new_turtle)
 
-x = -300
-y = 200
+x = -200
+y = 130
 
 for i in range(0, 6):
     list_of_turtles[i].teleport(x, y)
     y -= 50
 
-user_turtle = t.textinput("Turtle Racing Game", "How will be the winner of the race? Enter a number: ").lower()
+user_bet = t.textinput("Turtle Racing Game", "How will be the winner of the race? Enter a color: ").lower()
 
-pace = 0
+if user_bet:
+    is_race_on = True
 
-while pace < 25:
+while is_race_on:
+
     for turtle in list_of_turtles:
-        for _ in range(1):
-            turtle.forward(random.randint(10,20))
-            
-    pace += 1
+        if turtle.xcor() > 200:
+            is_race_on = False
+            winner_color = turtle.pencolor()
+            if winner_color == user_bet:
+                print(f"You win! The {winner_color} turtle is the winner.")
+            else:
+                print(f"You lose. The {winner_color} turtle is the winner.")
+        turtle.forward(r.randint(0, 10))
 
 screen.exitonclick()
